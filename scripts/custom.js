1458a4804f0fa759108067b797e08052c29649d5
kamlender <kamlender.devra@teamcomputers.com>
2021-03-21 01:11:11 +0530

    dashboard page

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,3 +1,29 @@
+/////////////// modal //////////////
+var modalBtns = [...document.querySelectorAll(".button")];
+modalBtns.forEach(function (btn) {
+  btn.onclick = function () {
+    var modal = btn.getAttribute("data-modal");
+    document.getElementById(modal).style.display = "block";
+  };
+});
+
+var closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function (btn) {
+  btn.onclick = function () {
+    var modal = btn.closest(".modal");
+    modal.style.display = "none";
+  };
+});
+
+window.onclick = function (event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
+};
+
+///////////////////////
+
+
 $(document).ready(function () {
   // header toggle menu
   const hamburger = document.querySelector('.hamburger');
